39de70db54253912abe8e0cb2d0a5d611dc8558b
ttheus27 <128244984+ttheus27@users.noreply.github.com>
2023-12-12 11:23:36 -0300

    Usando Warppers

---
+++ Mod_9/src/exemplos.java
@@ -0,0 +1,19 @@
+package Mod_9.src;
+
+public class exemplos {
+    private Short shor;
+
+    private Integer codigo;
+
+    private Long codigoMaior = 123455l;
+
+    private Double valorDecimal = (double) 10.5;
+
+    private Boolean status = false;
+
+    private Byte bi;
+
+    private Character letra =  'a';
+
+    
+}
