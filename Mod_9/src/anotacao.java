0ef0ea40cb4797fd412eec67fa2a5d35ec948522
ttheus27 <128244984+ttheus27@users.noreply.github.com>
2023-12-18 21:40:23 -0300

    è isso ai

---
+++ Mod_9/src/anotacao.java
@@ -0,0 +1,35 @@
+package Mod_9.src;
+
+public class anotacao {
+    // private Short shor;
+
+    // private Integer codigo;
+
+    // private Long codigoMaior = 123455l;
+
+    // private Double valorDecimal = (double) 10.5;
+
+    // private Boolean status = false;
+
+    // private Byte bi;
+
+    // private Character letra =  'a';
+
+
+    // Boxing (Autoboxing)
+    public static void main(String[] args) {
+        // Boolean status = true;
+        // Boolean statuson = Boolean.TRUE;
+
+
+        //casting explicito 
+        int num1 = 10;
+        short num2 = (short) num1;
+
+        
+    }
+
+    
+}
+
+
