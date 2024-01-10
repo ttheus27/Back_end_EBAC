fb7febc6468c45ea1a3c5115024a6511301e25a4
ttheus27 <128244984+ttheus27@users.noreply.github.com>
2024-01-10 11:05:34 -0300

    vendo Loop wih for,while, do while, break and continue

--- Mod_10/src/anotacao.java
+++ Mod_10/src/anotacao.java
@@ -5,7 +5,24 @@ import java.util.Scanner;
 public class anotacao {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        
+        System.out.println("Digite sua idade:");
+        int idade = s.nextInt();
+        String idadeSt = getIdade(idade);
+        System.out.println(idadeSt);
+        
+    }
 
+    public static String getIdade(int idade) {
+        if (idade >=0 && idade <= 5) {
+            return "Voce é um bebe";
+        } else if (idade >=6 && idade <=10) {
+            return "voce é uma criança";
+        } else if (idade >=11 && idade <=18) {
+            return "Voce é um adolecente";
+        } else {
+            return "Voce ja é um adulto";
+        }
         
     }
     
---
+++ Mod_10/src/anotacaoAula2.java
@@ -0,0 +1,14 @@
+package Mod_10.src;
+import java.util.Scanner;
+
+public class anotacaoAula2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite um numero para fazer tabuada:");
+           int num = s.nextInt();
+        for (int i = 0; i <= 10; i++){
+            System.out.println(num + " X " + i + " = " + num*i);
+        }
+    }
+}
