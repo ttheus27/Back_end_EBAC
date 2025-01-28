e3ee6aa20068a960cbd67f0b5a81e277e5b6a297
Matheus silva <theus.mss27@gmail.com>
2025-01-28 16:29:12 -0300

    mod 12

---
+++ Mod_12/src/Array.java
@@ -0,0 +1,26 @@
+package Mod_12.src;
+
+public class Array {
+    public static void main(String[] args) {
+        testeArray();
+        
+    }
+
+    public static void testeArray(){
+        int[] meuArray = new int[10];
+
+        meuArray [0] = 1;
+        meuArray [1] = 1;
+        meuArray [2] = 1;
+        meuArray [3] = 1;
+        meuArray [4] = 1;
+        meuArray [5] = 1;
+        meuArray [6] = 1;
+        meuArray [7] = 1;
+        meuArray [8] = 1;
+        meuArray [9] = 1;
+
+        System.out.println(meuArray);
+    }
+    
+}
